54552e95b11eba125ee815b1d589563be4c451a6
KyleRabkin61 <kylerabkin6161@gmail.com>
2024-06-08 14:12:12 -0400

    fixed imgs

--- script.js
+++ script.js
@@ -2,31 +2,31 @@ const products = [
     {
         name: 'Wooden Airplane',
         age: 'Toddlers',
-        img: '/imgs/plane1.jpg',
+        img: 'imgs/plane1.jpg',
         description: 'Soar through imaginative skies with this classic wooden airplane. Handcrafted from sustainable Baltic birch wood with a safe, natural harvest finish and a spinning propeller. Measures 3.5"H x 7"L x 7"W.'
     },
     {
         name: 'Wooden Train Set',
         age: '3+',
-        img: '/imgs/train1.jpg',
+        img: 'imgs/train1.jpg',
         description: 'Embark on a charming journey with this beautiful handcrafted wooden train set. Engine and three interchangeable cars boast intricate details made from real beech wood. Large size (84cm L x 11cm H x 13cm W) with moving wheels and a fully ecological design.'
     },
     {
         name: 'Wooden Boat',
         age: 'All ages',
-        img: '/imgs/boat1.jpg',
+        img: 'imgs/boat1.jpg',
         description: 'Set sail for bathtub adventures with this adorable wooden boat. Made from solid Maine white pine, this handcrafted toy floats and features rounded edges for safety. Includes two peg "lobster people." Size: 10.5"W x 3.5"H.'
     },
     {
         name: 'Wooden Block Set',
         age: '3+',
-        img: '/imgs/block1.jpg',
+        img: 'imgs/block1.jpg',
         description: 'Build creativity and imagination with this high-quality, 72-piece block set. Made from naturally finished and smooth-sanded hardwood blocks, this set comes in a convenient wooden storage crate (13” L x 12” W x 2” H).'
     },
     {
         name: 'Wooden Car',
         age: 'All ages',
-        img: '/imgs/car1.jpg',
+        img: 'imgs/car1.jpg',
         description: 'This heirloom-quality wooden car is a timeless treasure. Handcrafted from domestic and exotic hardwoods with a clear lacquer finish, this unique car will inspire generations of imaginative play. Please note potential choking hazards for small children.'
     }
 ]
